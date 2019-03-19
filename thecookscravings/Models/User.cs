ac4e3bf20970c6510479b7e50a8c3dbea17c851d
Andrew Moll <mollandrew913@gmail.com>
2019-03-19 21:00:08 +1300

    Add User class

---
+++ thecookscravings/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace thecookscravings.Models
+{
+    public class User
+    {
+        int Id { get; }
+        string FirstName { get; set; }
+        string LastName { get; set; } 
+        string Email { get; set; }
+        string Status { get; set; }
+        string Image { get; set; }
+
+
+    }
+}
